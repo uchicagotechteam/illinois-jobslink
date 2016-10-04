0fb67c91fc00dd414530865c74232b9d5dae6c34
Subhodh Kotekal <subhodh.kotekal@gmail.com>
2016-10-04 10:17:02 -0500

    chore(init_scrape): clean up old comments

--- init_scrape.py
+++ init_scrape.py
@@ -21,53 +21,47 @@ def scrape():
     r = session.get(SEARCH_URL)
     soup = BeautifulSoup(r.content, "html.parser")
     print(soup.prettify().encode('utf-8'))
+
+    # Print the urls for each job listing.
+    # Will need to implement stepping through these urls and scraping the
+    # relevant data.
     listings = soup.find_all("dt")
-    # for l in listings:
-    #     print(l)
-    #     urls = l.find_all('a')
-    #     for u in urls:
-    #         job_url = u['href']
-    #         print(BASE_URL + job_url)
+    for l in listings:
+        print(l)
+        urls = l.find_all('a')
+        for u in urls:
+            job_url = u['href']
+            print(BASE_URL + job_url)
 
 if __name__ == '__main__':
     session = requests.Session()
-    soup = BeautifulSoup(session.get(SEARCH_URL).content, "html.parser")
-    inputs = soup.find_all('input')
-    token = ''
-    for t in inputs:
-        try:
-            if t['name'] == 'authenticity_token':
-                token = t['value']
-                break
-        except KeyError as e:
-            pass
-    # print(soup.prettify().encode('utf-8'))
-    print(token)
 
-    login_data = dict(v_username=USER_NAME,
-                      v_password=PASSWORD,
-                      authenticity_token=token,
-                      commit='Log In')
-    login_data['utf-8'] = '&#x2713;'
+    # Code for Illinois Jobs Link Login - TODO: Fix login issues. (Try utf-8
+    # encoding??)
 
-    r = session.post(LOGIN_URL, data=login_data)
+    # soup = BeautifulSoup(session.get(SEARCH_URL).content, "html.parser")
+    # inputs = soup.find_all('input')
+    # token = ''
+    # for t in inputs:
+    #     try:
+    #         if t['name'] == 'authenticity_token':
+    #             token = t['value']
+    #             break
+    #     except KeyError as e:
+    #         pass
+    # # print(soup.prettify().encode('utf-8'))
+    # print(token)
 
-    print(r.content)
+    # login_data = dict(v_username=USER_NAME,
+    #                   v_password=PASSWORD,
+    #                   authenticity_token=token,
+    #                   FormName=0,
+    #                   fromlogin=1,
+    #                   button='Log+in')
+    # login_data['utf-8'] = '&#x2713;'
 
-    # r = session.get(
-    #     'https://illinoisjoblink.illinois.gov/ada/mn_loginstatistics_dsp.cfm')
+    # r = session.post(LOGIN_URL, data=login_data)
 
-    # soup = BeautifulSoup(r.content, 'html.parser')
-    # print(soup.prettify().encode('utf-8'))
+    # print(r.content)
 
-    # browser = RoboBrowser(history=True)
-    # browser.open(SEARCH_URL)
-    # forms = browser.get_forms()
-    # print(forms)
-    # login_form = forms[1]  # Hardcoded LOL
-    # login_form['v_username'] = USER_NAME
-    # login_form['v_password'] = PASSWORD
-    # browser.submit_form(login_form)
-
-    # print(browser.parsed)
-    # scrape()
+    scrape()
--- settings.py
+++ settings.py
@@ -1,17 +1,5 @@
 BASE_URL = 'https://illinoisjoblink.illinois.gov'
 SEARCH_URL = 'https://illinoisjoblink.illinois.gov/ada/r/search/jobs'
-CREATE_TABLE = 'https://www.googleapis.com/fusiontables/v2/tables'
-INIT_TABLE = {
-    "name": "Illinois JobsLink Postings",
-    "columns": [],
-    "isExportable": True
-}
-CLIENT_ID = "473717426781-1t89agdbn8r2k9nj2fdr77622aba0ltp.apps.googleusercontent.com"
-CLIENT_SECRET = "j-AqGBOqsEXVQBsWHcP8IE-_"
-API_KEY = "AIzaSyASH1rUfGB_L9VHQlbrvEXEeShnH5q7G10"
-TABLE_ID = "1UjAsnTg3R4-eJoRjEk3XLHrxQvCR09s6XKoOhswQ"
-SCOPES = ['https://www.googleapis.com/auth/sqlservice.admin',
-          'https://www.googleapis.com/auth/fusiontables']
 
 USER_NAME = 'subhodhkotekal'
 PASSWORD = 'techteam$1'
