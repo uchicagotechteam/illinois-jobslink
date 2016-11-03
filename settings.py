4a56cfa23c0ea31bab427c139500904d5f294e24
Subhodh Kotekal <subhodh.kotekal@gmail.com>
2016-11-02 19:39:03 -0500

    update creds

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ BASE_URL = 'https://illinoisjoblink.illinois.gov'
 SEARCH_URL = 'https://illinoisjoblink.illinois.gov/ada/r/search/jobs'
 
 USER_NAME = 'subhodhkotekal'
-PASSWORD = 'techteam$1'
+PASSWORD = 'techteam#1'
 LOGIN_URL = 'https://illinoisjoblink.illinois.gov/ada/mn_login_fnc.cfm'
 DB = 'listings.db'
